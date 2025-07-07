e6e3279bad37d0e77b5a57ef9af9f5964022f08f
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-07-07 22:24:29 +0530

    fixed responsiveness bugs

--- app/page.js
+++ app/page.js
@@ -13,13 +13,13 @@ export default function Home() {
       <section className="bg-[rgb(37,79,26)] flex flex-col md:grid grid-cols-2 px-[5vw] md:pt-[31vh] pt-[15vh] md:pb-[25vh] p-14" >
         <div className="content flex flex-col justify-start gap-7">
           <h1 className="md:text-7xl text-5xl font-black text-[#d2e823] ">Everything you are. In one, simple link in bio.</h1>
-          <div className="btns flex  gap-2 md:py-5 ">
-            <input value={text} onChange={(e) => { settext(e.target.value) }} className='bg-white text-[#867975] rounded-lg md:py-4.5 py-2.5 px-1.5 w-[43%] font-medium' type="text" placeholder="Enter your handle" />
-            <button onClick={() => { createTree() }} className='cursor-pointer hover:bg-fuchsia-400 bg-[#e9c0e9] text-[#1e2341] rounded-full  md:px-7  px-2.5 font-bold'>Claim your BitTree</button>
+          <div className="btns flex flex-col md:flex-row  gap-3 md:py-5 ">
+            <input value={text} onChange={(e) => { settext(e.target.value) }} className='bg-white text-[#867975] rounded-lg md:py-4.5 py-3 px-1.5 md:w-[43%] w-full font-medium' type="text" placeholder="Enter your handle" />
+            <button onClick={() => { createTree() }} className='cursor-pointer hover:bg-fuchsia-400 bg-[#e9c0e9] text-[#1e2341] rounded-full  md:px-7 py-2.5 w-fit px-5 font-bold'>Claim your BitTree</button>
           </div>
           <div className="lines text-white md:text-lg   text-sm font-medium">Join 70M+ people using BitTree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</div>
         </div>
-        <div className="gifs hidden md:block "><img className="pl-6 h-[105%] w-[110%]" src="./home.png" alt="" /></div>
+        <div className="gifs  py-10 md:p-0"><img className="pl-6 h-[105%] w-[110%]" src="./home.png" alt="" /></div>
       </section>
       <section className="bg-[#e9c0e9] gap-10 flex flex-col md:grid  grid-cols-2 px-[5vw] md:pt-[25vh] p-10 " >
         <div className="gifs "><img className="pl-6 h-[100%] w-[100%]" src="./home2.png" alt="" /></div>
