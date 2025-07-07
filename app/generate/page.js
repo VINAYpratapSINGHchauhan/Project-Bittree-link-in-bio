e6a7f3092c3b554ec21e219acdedb9f44611fc2d
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-07-07 23:05:05 +0530

    added responsiveness

--- app/generate/page.js
+++ app/generate/page.js
@@ -66,8 +66,8 @@ const generate = () => {
 
     return (<>
         <ToastContainer />
-        <div className="generate bg-[#F6F1DA] grid grid-cols-2  ">
-            <div className="col1 flex flex-col  px-[8vw] pt-[25vh]">
+        <div className="generate min-h-screen overflow-x-hidden  bg-[#F6F1DA] flex flex-col md:grid grid-cols-2 h-full ">
+            <div className="col1 flex flex-col  px-[8vw] md:pt-[25vh] pt-[20vh]">
 
                 <h1 className='text-5xl font-bold'>Create Your BitTree</h1>
 
@@ -86,10 +86,10 @@ const generate = () => {
                     <button onClick={() => { addLinks() }} className=' bg-[#502274] hover:bg-[rgb(30,35,48)] w-fit  text-white rounded-full py-2 px-3.5 text-sm font-bold'>+ Add Links</button>
                     <h2 className='font-semibold text-sm'>Step 3 : Add Picture and Finalize</h2>
                     <input value={pic} onChange={e => { setpic(e.target.value) }} className='w-[88%] mx-auto px-4 py-2 focus:outline-pink-900 bg-white rounded-2xl' type="text" placeholder='Enter link to your picture' />
-                    <button disabled={pic == "" || handle == "" || !Links[0] || Links[0].linktext === ""} onClick={() => { submitLinks() }} className=' bg-[#502274] hover:bg-[rgb(30,35,48)] w-[48%] mx-auto disabled:bg-gray-500 text-white rounded-full py-2 px-3.5 text-sm font-bold'>Create your BitTree</button>
+                    <button disabled={pic == "" || handle == "" || !Links[0] || Links[0].linktext === ""} onClick={() => { submitLinks() }} className=' bg-[#502274] hover:bg-[rgb(30,35,48)] mx-auto disabled:bg-gray-500 text-white rounded-full py-2 px-3.5 text-sm font-bold w-fit'>Create your BitTree</button>
                 </div>
             </div>
-            <div className="col2"><img src="https://assets.production.linktr.ee/auth/3289/media/banner-login-desktop.f355be949b508c58ec2d.webp" alt="Promotional Banner" className="h-[120vh] w-full object-cover" />
+            <div className="col2"><img src="https://assets.production.linktr.ee/auth/3289/media/banner-login-desktop.f355be949b508c58ec2d.webp" alt="Promotional Banner" className="max-h-screen w-full object-cover hidden md:block" />
             </div>
         </div>
     </>
